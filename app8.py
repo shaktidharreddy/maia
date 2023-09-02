d36250005a7b82ed670f5308f7465c4694c12f66
shaktidharreddy <109575010+shaktidharreddy@users.noreply.github.com>
2023-09-03 02:23:34 +0530

    Update app8.py
    updated removing the api_key in python file

--- app8.py
+++ app8.py
@@ -1,5 +1,6 @@
 #enhancing -current working version, modifying the pubmed chatbot with pandas df agent
 import os
+import openai
 import re
 import urllib
 import urllib.request
@@ -83,7 +84,7 @@ rootdir = "C:/Users/91961/maia/maia"
 datadir = "C:/Users/91961/maia/maia/data"
 promptdir = "C:/Users/91961/maia/maia/prompts"
 Entrez.email = "shakti20889@gmail.com"
-openai_api_key = "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"
+
 
 # def progress_bar_method(secs):
 #     # Code for your second asynchronous method goes here
@@ -578,6 +579,7 @@ def main():
         #logout button on main container
         authenticator.logout('Logout', 'main')
         st.subheader(f'Welcome *{st.session_state["name"]}*')
+        st.session_state.openai_api_key  = st.text_input("Enter your OpenAI API Key", '',type="password")
         
         #set bg image cover
         #set_bg_hack(os.path.join(rootdir, 'iqvia-dark-blue.png'))
@@ -760,7 +762,7 @@ def main():
 
             # Setup LLM and QA chain
             llm = ChatOpenAI(
-                model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, temperature=0, streaming=True
+                model_name="gpt-3.5-turbo", openai_api_key=st.session_state.openai_api_key, temperature=0, streaming=True
             )
             qa_chain = ConversationalRetrievalChain.from_llm(
                 llm, retriever=retriever, memory=memory, verbose=True
